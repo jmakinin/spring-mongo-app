b9e496704921b450c5f555443218beb877941fd3
jmakinin <36607363+jmakinin@users.noreply.github.com>
2023-06-02 05:04:11 +0000

    feat: added movies controller

---
+++ src/main/java/com/makinin/movies/MoviesController.java
@@ -0,0 +1,23 @@
+package com.makinin.movies;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/movies")
+public class MoviesController {
+
+    @Autowired
+    private MoviesService moviesService;
+
+    @GetMapping
+    public ResponseEntity<List<Movies>> getAllMovies() {
+        return new ResponseEntity<List<Movies>>(moviesService.allMovies(), HttpStatus.OK);
+    }
+}
