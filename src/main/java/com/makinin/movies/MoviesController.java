5ab784606c0e27d313772d8ef92358d625fb236c
jmakinin <36607363+jmakinin@users.noreply.github.com>
2023-06-02 19:30:22 +0000

    feat: single movie fetch controller

--- src/main/java/com/makinin/movies/MoviesController.java
+++ src/main/java/com/makinin/movies/MoviesController.java
@@ -1,13 +1,16 @@
 package com.makinin.movies;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/v1/movies")
@@ -19,5 +22,10 @@ public class MoviesController {
     @GetMapping
     public ResponseEntity<List<Movies>> getAllMovies() {
         return new ResponseEntity<List<Movies>>(moviesService.allMovies(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Movies>> getSingleMovie(@PathVariable ObjectId id) {
+        return new ResponseEntity<Optional<Movies>>(moviesService.singleMovie(id), HttpStatus.OK);
     }
 }
