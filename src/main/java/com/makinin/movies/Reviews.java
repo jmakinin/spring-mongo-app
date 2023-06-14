4a3fde4248a8f70eb87f346dd9d392ed40de62f0
jmakinin <36607363+jmakinin@users.noreply.github.com>
2023-06-14 10:21:27 +0000

    feat: reviews service

--- src/main/java/com/makinin/movies/Reviews.java
+++ src/main/java/com/makinin/movies/Reviews.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "reviews")
@@ -11,8 +12,12 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Reviews {
-
+    @Id
     private ObjectId id;
 
     private String body;
+
+    public Reviews(String body) {
+        this.body = body;
+    }
 }
